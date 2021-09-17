ef620523304eb71c472b147ea6a4505edd320f63
AyoCodess <ayo.daniel.adesanya@gmail.com>
2021-09-17 13:38:14 +0200

    added socials of other members

--- src/Card.js
+++ src/Card.js
@@ -29,28 +29,32 @@ const Card = ({
         <div className="card-text-content">
           <h1 className="card-title"> {title} </h1>
           <h2 className="card-name"> {name} </h2>
-          <p className="card-email">{email}</p>
-          <a href={website}>
+          <p className="card-email">
+            <a href={`mailto: ${email}`} target="_blank" rel="noreferrer">
+              {email}{" "}
+            </a>
+          </p>
+          <a href={website} target="_blank" rel="noreferrer">
             <span className="card-website">Website</span>
           </a>
           <div className="card-social-media-container">
             <ul>
               <li>
-                <a href={linkedin}>
+                <a href={linkedin} target="_blank" rel="noreferrer">
                   <span>
                     <FontAwesomeIcon icon={["fab", "linkedin"]} />
                   </span>
                 </a>
               </li>
               <li>
-                <a href={instagram}>
+                <a href={instagram} target="_blank" rel="noreferrer">
                   <span>
                     <FontAwesomeIcon icon={["fab", "instagram"]} />
                   </span>
                 </a>
               </li>
               <li>
-                <a href={twitter}>
+                <a href={twitter} target="_blank" rel="noreferrer">
                   <span>
                     <FontAwesomeIcon icon={["fab", "twitter"]} />
                   </span>
