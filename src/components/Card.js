a86125b3604b9eb521b3054a3f97dc00782e0d84
AyoCodess <ayo.daniel.adesanya@gmail.com>
2021-09-19 18:07:32 +0200

    update

--- src/components/Card.js
+++ src/components/Card.js
@@ -10,173 +10,186 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 library.add(fab);
 
 class Card extends Component {
+  constructor(
+    name,
+    email,
+    image,
+    id,
+    title,
+    website,
+    linkedin,
+    instagram,
+    twitter,
+    job,
+    location
+  ) {
+    super(props);
 
-    constructor (name, email, image, id, title, website, linkedin, instagram, twitter, job, location) {
+    name = this.name;
+    email = this.email;
+    image = this.image;
+    id = this.id;
+    title = this.title;
+    website = this.website;
+    linkedin = this.linkedin;
+    instagram = this.instagram;
+    twitter = this.instagram;
+    job = this.job;
+    location = this.location;
 
-        super(props);
+    this.state = {
+      hasName: false,
+      hasEmail: false,
+      hasImage: false,
+      hasId: false,
+      hasTitle: false,
+      hasWebsite: false,
+      hasLinkedin: false,
+      hasInstagram: false,
+      hasTwitter: false,
+      hasJob: false,
+      hasLocation: false,
+    };
+  }
 
-        name = this.name;
-        email = this.email ;
-        image = this.image;
-        id = this.id ;
-        title = this.title;
-        website = this.website;
-        linkedin = this.linkedin;
-        instagram = this.instagram;
-        twitter = this.instagram;
-        job = this.job;
-        location = this.location;
+  hasName() {
+    this.setState({ hasName: true });
+  }
 
-        this.state = {
-            hasName: false,
-            hasEmail: false,
-            hasImage: false,
-            hasId: false,
-            hasTitle: false,
-            hasWebsite: false,
-            hasLinkedin: false,
-            hasInstagram: false,
-            hasTwitter: false,
-            hasJob: false,
-            hasLocation: false
+  hasEmail() {
+    this.setState({ hasEmail: true });
+  }
 
-        };
+  hasId() {
+    this.setState({ hasId: true });
+  }
+
+  hasTitle() {
+    this.setState({ hasTitle: true });
+  }
+
+  hasWebsite() {
+    this.setState({ hasWebsite: true });
+  }
+
+  hasLinkedin() {
+    this.setState({ hasLinkedin: true });
+  }
+
+  hasInstagram() {
+    this.setState({ hasInstagram: true });
+  }
+
+  hasTwitter() {
+    this.setState({ hasTwitter: true });
+  }
+
+  hasJob() {
+    this.setState({ hasJob: true });
+  }
+  hasLocation() {
+    this.setState({ hasLocation: true });
+  }
+
+  render() {
+    const userHasName = this.state.hasName;
+    const userHasEmail = this.state.hasEmail;
+    const userHasImage = this.state.hasEmail;
+    const userHasId = this.state.hasId;
+    const userHasTitle = this.state.hasTitle;
+    const userHasWebsite = this.state.hasWebsite;
+    const userHasLinkedin = this.state.hasLinkedin;
+    const userHasInstagram = this.state.hasInstagram;
+    const userHasTwitter = this.state.hasTwitter;
+    const userHasJob = this.state.hasJob;
+    const userHasLocation = this.state.hasLocation;
+
+    if (userHasName && userHasImage) {
+      return (hasNameAndImage = (
+        <div className="card-image">
+          <img src={image} alt={name} />
+        </div>
+      ));
     }
 
-        hasName() {
-            this.setState({hasName: true})
-        }
+    if (userHasTitle && userHasName && userHasEmail) {
+      return (hasBasicInfo = (
+        <div className="card-text-content">
+          <h1 className="card-title"> {title} </h1>
+          <h2 className="card-name"> {name} </h2>
+          <p className="card-email">
+            <a href={`mailto: ${email}`} target="_blank" rel="noreferrer">
+              {email}
+            </a>
+          </p>
+          <p className="card-job">{job}</p>
+          <p className="card-location">{location}</p>
+        </div>
+      ));
+    }
 
-        hasEmail() {
-            this.setState({hasEmail: true})
-        }
+    if (userHasWebsite) {
+      return (userHasWebsite_ = (
+        <a href={website} target="_blank" rel="noreferrer">
+          <span className="card-website">(Click Me)</span>
+        </a>
+      ));
+    }
 
-        hasId() {
-            this.setState({hasId: true})
-        }
+    if (userHasLinkedin) {
+      return (userHasLinkedin_ = (
+        <li>
+          <a href={linkedin} target="_blank" rel="noreferrer">
+            <span>
+              <FontAwesomeIcon icon={["fab", "linkedin"]} />
+            </span>
+          </a>
+        </li>
+      ));
+    }
 
-        hasTitle() {
-            this.setState({hasTitle: true})
-        }
+    if (userHasInstagram) {
+      return (userHasInstagram_ = (
+        <li>
+          <a href={instagram} target="_blank" rel="noreferrer">
+            <span>
+              <FontAwesomeIcon icon={["fab", "instagram"]} />
+            </span>
+          </a>
+        </li>
+      ));
+    }
 
-        hasWebsite() {
-            this.setState({hasWebsite: true})
-        }
+    if (userHasTwitter) {
+      return (userHasTwitter_ = (
+        <li>
+          <a href={twitter} target="_blank" rel="noreferrer">
+            <span>
+              <FontAwesomeIcon icon={["fab", "twitter"]} />
+            </span>
+          </a>
+        </li>
+      ));
+    }
 
-        hasLinkedin() {
-            this.setState({hasLinkedin: true})
-        }
-
-        hasInstagram() {
-            this.setState({hasInstagram: true})
-        }
-
-        hasTwitter() {
-            this.setState({hasTwitter: true})
-        }
-
-        hasJob() {
-            this.setState({hasJob: true})
-        }
-        hasLocation() {
-            this.setState({hasLocation: true})
-        }
-
-        render() {
-            const  userHasName = this.state.hasName;
-            const  userHasEmail = this.state.hasEmail;
-            const  userHasImage = this.state.hasEmail;
-            const  userHasId = this.state.hasId;
-            const  userHasTitle = this.state.hasTitle;
-            const  userHasWebsite = this.state.hasWebsite;
-            const  userHasLinkedin = this.state.hasLinkedin;
-            const  userHasInstagram = this.state.hasInstagram;
-            const  userHasTwitter = this.state.hasTwitter;
-            const  userHasJob = this.state.hasJob;
-            const  userHasLocation = this.state.hasLocation;
-
-            if (userHasName && userHasImage) {
-                return hasNameAndImage =
-                <div className="card-image">
-                    <img src={image} alt={name} />
-
-                </div>
-
-            }
-
-            if (userHasTitle && userHasName && userHasEmail) {
-                return  hasBasicInfo =
-                <div className="card-text-content">
-                    <h1 className="card-title"> {title} </h1>
-                    <h2 className="card-name"> {name} </h2>
-                    <p className="card-email">
-                    <a href={`mailto: ${email}`} target="_blank" rel="noreferrer">
-                        {email}
-                    </a>
-                    </p>
-                    <p className="card-job">{job}</p>
-                    <p className="card-location">{location}</p>
-                </div>
-
-                }
-
-                if (userHasWebsite) {
-                    return userHasWebsite_ = <a href={website} target="_blank" rel="noreferrer">
-                    <span className="card-website">(Click Me)</span>
-                  </a>
-                }
-
-                if (userHasLinkedin) {
-                    return userHasLinkedin_ =
-                    <li>
-                    <a href={linkedin} target="_blank" rel="noreferrer">
-                      <span>
-                        <FontAwesomeIcon icon={["fab", "linkedin"]} />
-                      </span>
-                    </a>
-                  </li>
-                }
-
-                if (userHasInstagram) {
-                    return userHasInstagram_ =
-                    <li>
-                  <a href={instagram} target="_blank" rel="noreferrer">
-                    <span>
-                      <FontAwesomeIcon icon={["fab", "instagram"]} />
-                    </span>
-                  </a>
-                </li>
-                }
-
-                if (userHasTwitter) {
-                    return userHasTwitter_ =
-                    <li>
-                  <a href={twitter} target="_blank" rel="noreferrer">
-                    <span>
-                      <FontAwesomeIcon icon={["fab", "twitter"]} />
-                    </span>
-                  </a>
-                </li>
-                }
-
-                return (
-                <>
-                    <div className=" grow card-body">
-                    {hasNameAndImage}
-                    {hasBasicInfo}
-                    {userHasWebsite_}
-                        <div className="card-social-media-container">
-                        <ul>
-                        {userHasLinkedin_}
-                        {userHasInstagram_ }
-                        {userHasTwitter_ }
-                        </ul>
-                        </div>
-                    </div>
-
-                </>
-                );
-  };
+    return (
+      <>
+        <div className=" grow card-body">
+          {hasNameAndImage}
+          {hasBasicInfo}
+          {userHasWebsite_}
+          <div className="card-social-media-container">
+            <ul>
+              {userHasLinkedin_}
+              {userHasInstagram_}
+              {userHasTwitter_}
+            </ul>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
 
 // const Card = ({
 //   name,
@@ -207,14 +220,14 @@ class Card extends Component {
 //           </p>
 //           <p className="card-job">{job}</p>
 //           <p className="card-location">{location}</p>
-        
+
 //           {
 //                 if (website !== "") {
 //                 <a href={website} target="_blank" rel="noreferrer">
 //                     <span className="card-website">Website</span>
-//                 </a> 
+//                 </a>
 //                 }
-//              } 
+//              }
 
 //           <div className="card-social-media-container">
 //             <ul>
