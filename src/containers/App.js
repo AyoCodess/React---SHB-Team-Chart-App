feebc941f51edeb029e08c9a1f3eb4cf9d3c5183
AyoCodess <ayo.daniel.adesanya@gmail.com>
2021-09-20 10:06:15 +0200

    changed app.js to a funnction

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useMemo } from "react";
 import Searchbox from "../components/Searchbox";
 import CardList from "../components/CardList";
 import Scroll from "../components/Scroll";
@@ -6,66 +6,51 @@ import { people } from "../data/people";
 import "../scss/containers/app.scss";
 import ErrorBoundary from "../components/errorBoundary";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      people: [],
-      searchfield: "",
-    };
-  }
+const App = () => {
+  const [searchField, setSearchField] = useState("");
 
-  componentDidMount() {
-    this.setState({ people: people });
-  }
+  const filteredPeople = useMemo(
+    () =>
+      people.filter((people) => {
+        return (
+          people.name.toLowerCase().includes(searchField.toLowerCase()) ||
+          people.title.toLowerCase().includes(searchField.toLowerCase())
+        );
+      }),
+    [searchField]
+  );
 
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value });
-  };
-
-  render() {
-    const filteredPeople = this.state.people.filter((people) => {
-      return (
-        people.name
-          .toLowerCase()
-          .includes(this.state.searchfield.toLowerCase()) ||
-        people.title
-          .toLowerCase()
-          .includes(this.state.searchfield.toLowerCase())
-      );
-    });
-
-    return (
-      <>
-        <div className="main-app tc">
-          <div class="header">
-            <h1 className=" app-title tc ">SHB Organisation</h1>
-            <h2 className=" app-title app-title--sub tc">Find a Mentor</h2>
-            <p className="app-description">
-              Feel free to contact any of us if you need support or have any
-              questions.
-            </p>
-          </div>
-          <Searchbox searchChange={this.onSearchChange} />
-          <Scroll>
-            <ErrorBoundary>
-              <CardList people={filteredPeople} />;
-            </ErrorBoundary>
-          </Scroll>
-          <footer>
-            <p className=" tc footer-title">
-              <a
-                href="https://www.stopholdingback.org"
-                target="_blank"
-                rel="noreferrer"
-              >
-                stopholdingback.org
-              </a>
-            </p>
-          </footer>
+  return (
+    <>
+      <div className="main-app tc">
+        <div class="header">
+          <h1 className=" app-title tc ">SHB Organisation</h1>
+          <h2 className=" app-title app-title--sub tc">Find a Mentor</h2>
+          <p className="app-description">
+            Feel free to contact any of us if you need support or have any
+            questions.
+          </p>
         </div>
-      </>
-    );
-  }
-}
+        <Searchbox searchField={searchField} searchChange={setSearchField} />
+        <Scroll>
+          <ErrorBoundary>
+            <CardList people={filteredPeople} />;
+          </ErrorBoundary>
+        </Scroll>
+        <footer>
+          <p className=" tc footer-title">
+            <a
+              href="https://www.stopholdingback.org"
+              target="_blank"
+              rel="noreferrer"
+            >
+              stopholdingback.org
+            </a>
+          </p>
+        </footer>
+      </div>
+    </>
+  );
+};
+
 export default App;
